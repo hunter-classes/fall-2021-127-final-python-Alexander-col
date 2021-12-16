6f12122492d8ecf0594691df703636273367ce15
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2021-12-16 18:28:33 +0000

    final push

--- answers.py
+++ answers.py
@@ -18,7 +18,6 @@ def isIncreasing(num_list):
       same = same + 1
   base = i
 
-  print (up_count, down_count, same, total)
   if down_count > 0 :
     return False
   else:
@@ -39,24 +38,30 @@ def numConvert(num_list):
   result = (int(result) )
   return (result)
 
-# Assume all items in the list are positive single digits.
+def binConvert(b_num):
+  result = 0 
+  val = 1
+  for i in b_num[::-1]: 
+      if i == '1':
+        result  =val**2 + result
+  return result
 
-# If you are totally stuck on this, DM me on Zulip and I will provide a hint but you won’t be able to get full credit if you take the hint.
-
-# ------------------- Question 3 ----------------
-# some brief description of what the output should be
-
-ex_up = [1,2,3,4,23,6,7,8,9]
+ex_up = [1,2,3,4,5,6,7,8,9]
 ex_down = [9,8,7,6,5,4,3,2,1]
 ex_2 = [3,5,1,1,5,3]
-print(isIncreasing(ex_up))
-print(isIncreasing(ex_down))
+
 
 print(''' ------------------- Question 1 ----------------
 There is a counter that keep track of the values that incresed and those that decreased. If there is a single value that decreased the the function will return false.''')
+print("When the function isIncreasing is given the increasing values of", ex_up,"then it will return", isIncreasing(ex_up))
+print("when the function is isIncreasing is give the values of dcreasing numbers of", ex_down, "then it will return", isIncreasing(ex_down))
 
 # question2 presentation and response
 print('''------------------- Question 2 ----------------
 The list of numbers are turned into strings and then they are put together and the final result is then turned back into an integer!''')
 print('when given the list numbers of',ex_2,"the numConvert functiion then turns it into", numConvert(ex_2))
+
+print('''------------------- Question 2 ----------------
+Attempted to move the numbers into reverse order and then square them in the case that they were a 1 and add it to result''')
+print(binConvert("1011"))
 
